195530a12b42c7a39dacd093e62236fb162c725c
Benjamin Eckel <bhelx@simst.im>
2023-07-04 12:03:27 -0500

    Revert "feat(poller): add poll on start-up, update corresponding tests"
    This reverts commit aa9f49f230c347707401203e1000b9a140e5e9ea.

--- poller.go
+++ poller.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 )
 
@@ -23,32 +22,21 @@ type PollerConfig struct {
 	Interval time.Duration
 }
 
-// Poll polls for results on an interval.
+// Poll fetches results on an interval and publishes the results.
 func (p Poller[U]) Poll(ctx context.Context) error {
 	tic := time.NewTicker(p.Config.Interval)
 	defer tic.Stop()
-	if err := p.poll(ctx); err != nil {
-		p.Log.Printf("ERROR: startup poll failed: %s", err)
-	}
 	for {
 		select {
 		case <-tic.C:
-			if err := p.poll(ctx); err != nil {
-				p.Log.Printf("ERROR: poll failed: %s", err)
+			results, err := p.APIClient.Load(ctx)
+			if err != nil {
+				p.Log.Printf("ERROR: apiclient failed to load: %s", err)
 				continue
 			}
+			p.Publisher.Publish(ctx, results)
 		case <-ctx.Done():
 			return ctx.Err()
 		}
 	}
-}
-
-// poll fetches results on an interval and publishes the results.
-func (p Poller[U]) poll(ctx context.Context) error {
-	results, err := p.APIClient.Load(ctx)
-	if err != nil {
-		return fmt.Errorf("apiclient failed to load: %w", err)
-	}
-	p.Publisher.Publish(ctx, results)
-	return nil
 }
--- poller_test.go
+++ poller_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"io"
 	"log"
 	"testing"
 	"time"
@@ -29,34 +28,99 @@ func (s apiClientStub[U]) Load(context.Context) (U, error) {
 	return s.result, s.err
 }
 
-func TestPoller_poll_success(t *testing.T) {
-	var gotLog bytes.Buffer
-	pStub := new(publisherStub[[]int])
-	err := Poller[[]int]{
-		Config: PollerConfig{
-			Interval: 10 * time.Millisecond,
-		},
-		APIClient: apiClientStub[[]int]{
-			result: []int{1, 2, 3},
-			err:    nil,
-		},
-		Log:       log.New(&gotLog, "", 0),
-		Publisher: pStub,
-	}.poll(context.Background())
-	if err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	if want := ""; gotLog.String() != want {
-		t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
-	}
-	if want := [][]int{{1, 2, 3}}; !cmp.Equal(pStub.results, want) {
-		t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub.results, want))
-	}
+func TestPoller_Poll_success(t *testing.T) {
+	t.Run("no poll", func(t *testing.T) {
+		// intervals determine the number of polls
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+		defer cancel()
+		var gotLog bytes.Buffer
+		pStub := new(publisherStub[[]int])
+		err := Poller[[]int]{
+			Config: PollerConfig{
+				Interval: 10 * time.Millisecond,
+			},
+			APIClient: apiClientStub[[]int]{
+				result: []int{1, 2, 3},
+				err:    nil,
+			},
+			Log:       log.New(&gotLog, "", 0),
+			Publisher: pStub,
+		}.Poll(ctx)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatalf("unexpected err: %v", err)
+		}
+		if want := ""; gotLog.String() != want {
+			t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
+		}
+		var want [][]int
+		if !cmp.Equal(pStub.results, want) {
+			t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub.results, want))
+		}
+	})
+	t.Run("single poll", func(t *testing.T) {
+		// intervals determine the number of polls
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Millisecond)
+		defer cancel()
+		var gotLog bytes.Buffer
+		pStub := new(publisherStub[[]int])
+		err := Poller[[]int]{
+			Config: PollerConfig{
+				Interval: 10 * time.Millisecond,
+			},
+			APIClient: apiClientStub[[]int]{
+				result: []int{1, 2, 3},
+				err:    nil,
+			},
+			Log:       log.New(&gotLog, "", 0),
+			Publisher: pStub,
+		}.Poll(ctx)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatalf("unexpected err: %v", err)
+		}
+		if want := ""; gotLog.String() != want {
+			t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
+		}
+		if want := [][]int{{1, 2, 3}}; !cmp.Equal(pStub.results, want) {
+			t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub.results, want))
+		}
+	})
+	t.Run("two times poll", func(t *testing.T) {
+		// intervals determine the number of polls
+		ctx, cancel := context.WithTimeout(context.Background(), 25*time.Millisecond)
+		defer cancel()
+		var gotLog bytes.Buffer
+		pStub := new(publisherStub[[]int])
+		err := Poller[[]int]{
+			Config: PollerConfig{
+				Interval: 10 * time.Millisecond,
+			},
+			APIClient: apiClientStub[[]int]{
+				result: []int{1, 2, 3},
+				err:    nil,
+			},
+			Log:       log.New(&gotLog, "", 0),
+			Publisher: pStub,
+		}.Poll(ctx)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatalf("unexpected err: %v", err)
+		}
+		if want := ""; gotLog.String() != want {
+			t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
+		}
+		if want := [][]int{{1, 2, 3}, {1, 2, 3}}; !cmp.Equal(pStub.results, want) {
+			t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub.results, want))
+		}
+	})
 }
 
-func TestPoller_poll_fail(t *testing.T) {
+func TestPoller_Poll_fail(t *testing.T) {
 	errFailed := errors.New("failed")
 	t.Run("apiclient err", func(t *testing.T) {
+		// intervals determine the number of polls
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Millisecond)
+		defer cancel()
+		var gotLog bytes.Buffer
+		pStub := new(publisherStub[[]int])
 		err := Poller[[]int]{
 			Config: PollerConfig{
 				Interval: 10 * time.Millisecond,
@@ -65,11 +129,48 @@ func TestPoller_poll_fail(t *testing.T) {
 				result: []int{1, 2, 3},
 				err:    errFailed,
 			},
-			Log:       log.New(io.Discard, "", 0),
-			Publisher: new(publisherStub[[]int]),
-		}.poll(context.Background())
-		if !errors.Is(err, errFailed) {
-			t.Fatalf("mismatch err: %v", err)
+			Log:       log.New(&gotLog, "", 0),
+			Publisher: pStub,
+		}.Poll(ctx)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatalf("unexpected err: %v", err)
+		}
+		wantLog := "ERROR: apiclient failed to load: failed\n"
+		if want := wantLog; gotLog.String() != want {
+			t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
+		}
+		var want [][]int
+		if !cmp.Equal(pStub.results, want) {
+			t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub, want))
+		}
+	})
+	t.Run("two apiclient err", func(t *testing.T) {
+		// intervals determine the number of polls
+		ctx, cancel := context.WithTimeout(context.Background(), 25*time.Millisecond)
+		defer cancel()
+		var gotLog bytes.Buffer
+		pStub := new(publisherStub[[]int])
+		err := Poller[[]int]{
+			Config: PollerConfig{
+				Interval: 10 * time.Millisecond,
+			},
+			APIClient: apiClientStub[[]int]{
+				result: []int{1, 2, 3},
+				err:    errFailed,
+			},
+			Log:       log.New(&gotLog, "", 0),
+			Publisher: pStub,
+		}.Poll(ctx)
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatalf("unexpected err: %v", err)
+		}
+		wantLog := "ERROR: apiclient failed to load: failed\nERROR: apiclient failed to load: failed\n"
+		if want := wantLog; gotLog.String() != want {
+			t.Errorf("mismatch log. diff: %v", cmp.Diff(gotLog.String(), want))
+		}
+		var want [][]int
+		if !cmp.Equal(pStub.results, want) {
+			t.Errorf("mismatch results. diff: %v", cmp.Diff(pStub, want))
 		}
 	})
 }
