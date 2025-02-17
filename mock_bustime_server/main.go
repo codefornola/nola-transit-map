78202043cfcf75f10b9025fcf265c580444c9a83
naf <naf@sdf.org>
2025-02-17 08:18:22 -0600

    Add JP Transit data to mock server

--- mock_bustime_server/main.go
+++ mock_bustime_server/main.go
@@ -15,6 +15,13 @@ func main() {
 			log.Println("GET /getvehicles :: Served Vehicles")
 		},
 	)
+        http.HandleFunc(
+                "/vehicles_jp",
+                func(w http.ResponseWriter, r *http.Request) {
+                        http.ServeFile(w, r, "./mock_vehicles_jp.protobuf")
+                        log.Println("GET /vehicles_jp :: Served Vehicles")
+                },
+        )
 	log.Printf("\n\nMock Clever Devices Bustime server is running at: http://localhost%s \n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
